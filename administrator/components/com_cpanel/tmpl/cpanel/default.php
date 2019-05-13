3b0cf568f58ceaebdab17f0f2fb24372de970a0d
Franciska Perisa <franci_perisa@hotmail.de>
2019-05-13 11:20:02 +0200

    Show messsage when module was added into the dashboard. Fixed access permission control.

--- administrator/components/com_cpanel/tmpl/cpanel/default.php
+++ administrator/components/com_cpanel/tmpl/cpanel/default.php
@@ -16,12 +16,11 @@ use Joomla\CMS\Language\Text;
 use Joomla\CMS\Router\Route;
 
 HTMLHelper::_('behavior.core');
+Text::script('COM_CPANEL_MSG_MODULE_ADDED');
 Text::script('COM_CPANEL_UNPUBLISH_MODULE_SUCCESS');
 Text::script('COM_CPANEL_UNPUBLISH_MODULE_ERROR');
 
-HTMLHelper::_('script', 'com_cpanel/admin-cpanel-default.min.js', array('version' => 'auto', 'relative' => true));
-
-$user = Factory::getUser();
+HTMLHelper::_('script', 'com_cpanel/admin-cpanel-default.js', ['version' => 'auto', 'relative' => true]);
 HTMLHelper::_('script', 'com_cpanel/admin-add_module.js', ['version' => 'auto', 'relative' => true]);
 
 // Set up the bootstrap modal that will be used for all module editors
@@ -34,9 +33,11 @@ echo HTMLHelper::_(
 		'url'         => Route::_('index.php?option=com_cpanel&task=addModule&function=jSelectModuleType&position=' . $this->escape($this->position)),
 		'bodyHeight'  => '70',
 		'modalWidth'  => '80',
-		'footer'      => '<button type="button" class="button-cancel  btn btn-sm btn-danger" data-dismiss="modal" data-target="#closeBtn" aria-hidden="true"><span class="icon-cancel" aria-hidden="true"></span>'
+		'footer'      => '<button type="button" class="button-cancel  btn btn-sm btn-danger" data-dismiss="modal" data-target="#closeBtn" aria-hidden="true">'
+            . '<span class="icon-cancel" aria-hidden="true"></span>'
 			. Text::_('JLIB_HTML_BEHAVIOR_CLOSE') . '</button>'
-			. '<button type="button" class="button-save  btn btn-sm btn-success hidden" data-target="#saveBtn" aria-hidden="true"><span class="icon-save" aria-hidden="true"></span>'
+			. '<button type="button" class="button-save  btn btn-sm btn-success hidden" data-target="#saveBtn" aria-hidden="true">'
+            . '<span class="icon-save" aria-hidden="true"></span>'
 			. Text::_('JSAVE') . '</button>',
 	)
 );
@@ -45,11 +46,11 @@ echo HTMLHelper::_(
 <?php if ($this->quickicons) : ?>
 <div class="cpanel-modules <?php echo $this->position; ?>-quickicons">
 	<div class="card-columns">
-    	<?php // Display the icon position modules
-		foreach ($this->quickicons as $iconmodule)
-		{
-			echo ModuleHelper::renderModule($iconmodule, array('style' => 'quickicon'));
-		}
+    <?php // Display the icon position modules
+    foreach ($this->quickicons as $iconmodule)
+    {
+        echo ModuleHelper::renderModule($iconmodule, array('style' => 'quickicon'));
+    }
 	?>
 	</div>
 </div>
@@ -57,26 +58,25 @@ echo HTMLHelper::_(
 
 <div class="cpanel-modules <?php echo $this->position; ?>">
 	<div class="card-columns">
-
 	<?php
 	foreach ($this->modules as $module)
 	{
 		echo ModuleHelper::renderModule($module, array('style' => 'well'));
 	}
 	?>
-	<?php if ($user->authorise('core.create', 'com_modules')) : ?>
-
 	</div>
 </div>
 
+<?php if (Factory::getApplication()->getIdentity()->authorise('core.manage', 'com_modules')) : ?>
 <div class="row">
 	<div class="col-md-6">
-		<a href="#moduleEditModal" data-toggle="modal" data-target="#moduleDashboardAddModal" role="button" class="cpanel-add-module text-center py-5 w-100 d-block">
+		<a href="#moduleEditModal" data-toggle="modal" data-target="#moduleDashboardAddModal"
+           role="button" class="cpanel-add-module text-center py-5 w-100 d-block">
 			<div class="cpanel-add-module-icon text-center">
 				<span class="fa fa-plus-square text-light mt-2"></span>
 			</div>
 			<span><?php echo Text::_('COM_CPANEL_ADD_DASHBOARD_MODULE'); ?></span>
 		</a>
 	</div>
-	<?php endif; ?>
 </div>
+<?php endif; ?>
--- build/media_source/com_cpanel/js/admin-add_module.es6.js
+++ build/media_source/com_cpanel/js/admin-add_module.es6.js
@@ -2,57 +2,49 @@
  * @copyright  Copyright (C) 2005 - 2019 Open Source Matters, Inc. All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
+
 Joomla = window.Joomla || {};
 
 ((document) => {
   'use strict';
 
   document.addEventListener('DOMContentLoaded', () => {
-    window.jSelectModuleType = () => {
-      const elements = [].slice.call(document.querySelectorAll('#moduleDashboardAddModal .modal-footer .btn.hidden'));
+    const modal = document.querySelector('#moduleDashboardAddModal');
+    const hiddenButtons = modal.querySelectorAll(':scope .modal-footer .btn.hidden');
 
-      if (elements.length) {
-        elements.forEach((button) => {
-          button.classList.remove('hidden');
-        });
-      }
+    // Show 'save & close'-button in modal after selecting a module from the list
+    window.jSelectModuleType = () => {
+      Array.prototype.forEach.call(hiddenButtons, (hidden) => {
+        hidden.classList.remove('hidden');
+      });
     };
 
-    const buttons = [].slice.call(document.querySelectorAll('#moduleDashboardAddModal .modal-footer .btn'));
-    const hideButtons = [];
+    // Pass on click to data-targets of modal footer buttons
+    Array.prototype.forEach.call(modal.querySelectorAll(':scope .modal-footer .btn'), (button) => {
+      button.addEventListener('click', (event) => {
+        // There is some bug with events in iframe where currentTarget is "null"
+        // => prevent this here by bubble up
+        const elem = event.currentTarget || event.target;
+        const target = elem.getAttribute('data-target');
+        const iframe = modal.querySelector('iframe');
+        const content = iframe.contentDocument || iframe.contentWindow.document;
+        const clickTarget = content.querySelector(target);
 
-    if (buttons.length) {
-      buttons.forEach((button) => {
-        if (button.classList.contains('hidden')) {
-          hideButtons.push(button);
+        if (clickTarget) {
+          clickTarget.click();
+
+          if (target === '#saveBtn') {
+            Joomla.renderMessages({ success: [Joomla.JText._('COM_CPANEL_MSG_MODULE_ADDED')] });
+          }
         }
-
-        button.addEventListener('click', (event) => {
-          let elem = event.currentTarget;
-
-          // There is some bug with events in iframe where currentTarget is "null"
-          // => prevent this here by bubble up
-          if (!elem) {
-            elem = event.target;
-          }
-
-          if (elem) {
-            const clickTarget = elem.getAttribute('data-target');
-
-            const iframe = document.querySelector('#moduleDashboardAddModal iframe');
-            const content = iframe.contentDocument || iframe.contentWindow.document;
-
-            content.querySelector(clickTarget).click();
-          }
-        });
       });
-    }
+    });
 
     // @TODO remove jQuery dependency, when the modal is not bootstrap anymore
     /* global jQuery */
     jQuery('#moduleDashboardAddModal').on('hide.bs.modal', () => {
-      hideButtons.forEach((button) => {
-        button.classList.add('hidden');
+      Array.prototype.forEach.call(hiddenButtons, (hide) => {
+        hide.classList.add('hidden');
       });
     });
   });
